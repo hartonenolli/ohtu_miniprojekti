06235564be8be2e93458319990b73aa4ae2af825
katajak <101673344+katajak@users.noreply.github.com>
2022-11-28 10:39:08 +0200

    AppLibrary keywords

--- src/AppLibrary.py
+++ src/AppLibrary.py
@@ -1,5 +1,26 @@
 from app import CommandLineUI
 
+class StubIO:
+    def __init__(self, inputs=None):
+        self.inputs = inputs or []
+        self.outputs = []
+
+    def read(self, prompt):
+        return self.inputs.pop(0)
+    
+    def write(self, prompt):
+        self.outputs.append(prompt)
+
+    def add_input(self, value):
+        self.inputs.append(value)
+
 class AppLibrary:
     def __init__(self):
         self._app = CommandLineUI()
+        self._io = StubIO()
+
+    def input(self, value):
+        self._io.add_input(value)
+
+    def start_app(self):
+        self._app.start_app()
