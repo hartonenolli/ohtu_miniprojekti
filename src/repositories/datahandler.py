a7a2b62d456500891a089131a89ba6caef7d30f3
HYTapio <tapio.uzal@helsinki.fi>
2022-12-09 11:13:08 +0200

    deleted all old methods

--- src/repositories/datahandler.py
+++ src/repositories/datahandler.py
@@ -1,33 +1,10 @@
 import bibtexparser
 from bibtexparser.bwriter import BibTexWriter
-from bibtexparser.bibdatabase import BibDatabase
 from bibtexparser.bparser import BibTexParser
 
 class BibtexHandler:
     def __init__(self):
         pass
-
-    def _create_bibtex_format_humanformat(self, data):
-        bibtex_entry = BibDatabase()
-        bibtex_entry.entries = [
-            {"title": str(data[1]),
-            "author": str(data[2]),
-            "year": str(data[3]),
-            "publisher": str(data[4]),
-            "ID": str(data[5]),
-            "ENTRYTYPE": "book"}]
-        return bibtex_entry
-
-    def write_to_bib_file_humanformat(self, data, file):
-        bibtex = self._create_bibtex_format_humanformat(data)
-        writer = BibTexWriter()
-        writer.indent = "    "
-        try:
-            with open(file, "a", encoding="utf-8") as bibfile:
-                bibfile.write(writer.write(bibtex))
-            return True
-        except PermissionError:
-            return False
 
     def _create_bibtex_format_bibtexformat(self, bibtex):
         bibparser = BibTexParser(interpolate_strings=False)
@@ -53,12 +30,8 @@ class BibtexHandler:
         except PermissionError:
             return None
 
-# Uusi kirjoitusfunktio. Reference-olio luo itse itsestään
-# bibtex-entryn oikeassa formaatissa tyypistä
-# (kirja, artikkeli jne. riippuen), ja tämä funktio hoitaa
-# vain kirjoittamisen.
 
-    def write_to_bib_file_humanformat_new(self, entry, file):
+    def write_to_bib_file_humanformat(self, entry, file):
         writer = BibTexWriter()
         writer.indent = "    "
         try:
--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -1,4 +1,3 @@
-from entities.book import Book
 from entities.reference import Reference
 
 class ReferenceServices:
@@ -6,28 +5,6 @@ class ReferenceServices:
         self._io = io
         self._bibhandler = bibhandler
         self.filename = filename
-
-    def add_reference_humanformat(self, reference):
-        self._io.write("Syötä viitteen tiedot:")
-        title = self._io.read("Nimi: ")
-        author = self._io.read("Kirjoittaja/t: ")
-        while True:
-            try:
-                year = int(self._io.read("Julkaisuvuosi (jätä tyhjäksi jos ei ole): "))
-                break
-            except ValueError:
-                self._io.write("Virheellinen syöte")
-        publisher = self._io.read("Kustantaja: ")
-        keyword = self._io.read("Avainsana, jolla haluat viitata teokseen: ")
-        text = f"Lisätään {reference} {title} ({year}), kirjoittanut {author}, "  \
-        f"julkaissut {publisher}, avainsanalla {keyword}"
-        self._io.write(text)
-
-        data = (reference, title, author, year, publisher, keyword)
-        if self._bibhandler.write_to_bib_file_humanformat(data, self.filename):
-            self._io.write("BibTex tiedoston kirjoittaminen onnistui")
-        else:
-            self._io.write("BibTex tiedoston kirjoittaminen epäonnistui")
 
     def delete_reference(self,reference):
         pass
@@ -39,16 +16,9 @@ class ReferenceServices:
         else:
             self._io.write("BibTex tiedoston kirjoittaminen epäonnistui")
 
+
+
     def list_references(self):
-        references = self._bibhandler.read_from_bib_file(self.filename)
-        refs = []
-        for entry in references.entries:
-            book = Book(entry["author"], entry["title"], entry["publisher"], entry["year"])
-            refs.append(book)
-        return refs
-
-
-    def list_references_new(self):
         references = self._bibhandler.read_from_bib_file(self.filename)
         refs = []
         for entry in references.entries:
@@ -73,15 +43,7 @@ class ReferenceServices:
             refs.append(reference)
         return refs
 
-# Uusi funktio viittauksen lisäämiseen. Entry-typestä
-# riippuen kysytään oikeat attribuutit. Loppuihin tulee None.
-# Funktio kutsuu reference-olion create_bibtex_entry metodia,
-# jonka paluuarvona on suoraan bibtex-tallennettava formaatti.
-# Paluuarvo viedään bibhandlerille, joka ainoastaan kirjoittaa.
-# HUOM käyttö rikkoo testejä atm. List_references ei
-# vielä osaa lukea kaikkia attribuutteja.
-
-    def add_reference_humanformat_new(self, entry_type):
+    def add_reference_humanformat(self, entry_type):
         self._io.write("Syötä viitteen tiedot:")
         key = self._io.read("Avainsana, jolla haluat viitata teokseen: ")
         title = self._io.read("Nimi: ")
@@ -106,7 +68,7 @@ class ReferenceServices:
             year, None, None, None, None, note)
         self._io.write("Lisätään " + str(reference))
         entry = reference.create_bibtex_entry()
-        if self._bibhandler.write_to_bib_file_humanformat_new(entry, self.filename):
+        if self._bibhandler.write_to_bib_file_humanformat(entry, self.filename):
             self._io.write("BibTex tiedoston kirjoittaminen onnistui")
         else:
             self._io.write("BibTex tiedoston kirjoittaminen epäonnistui")
