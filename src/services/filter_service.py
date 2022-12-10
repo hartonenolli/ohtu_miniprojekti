2528418d29acbc113f04cacf9873c956efb93ceb
Teemu Tiljander <teemuti@gmail.com>
2022-12-10 17:47:19 +0200

    Added filtering and sorting

---
+++ src/services/filter_service.py
@@ -0,0 +1,64 @@
+from bibtexparser.bibdatabase import BibDatabase
+class FilterService():
+    def __init__(self) -> None:
+        pass
+
+    def sort_by_year(self, bib_database, descending=False):
+        if descending:
+            bib_database.entries.sort(key= lambda x : x["year"], reverse=True)
+            return bib_database
+        bib_database.entries.sort(key = lambda x : x["year"])
+        return bib_database
+    
+    def filter_by_year(self, bib_database, year : int):
+        bib_result_database = BibDatabase()
+        bib_result_database.entries = [n for n in bib_database.entries if int(n["year"]) == year]
+        return bib_result_database
+    
+    def sort_by_author(self, bib_database, descending=False):
+        if descending:
+            bib_database.entries.sort(key= lambda x : x["author"], reverse=True)
+            return bib_database
+        bib_database.entries.sort(key = lambda x : x["author"])
+        return bib_database
+    
+    def filter_by_author(self, bib_database, author : str):
+        bib_result_database = BibDatabase()
+        bib_result_database.entries = [n for n in bib_database.entries if ["author"].lower() == author.lower()]
+        return bib_result_database
+
+    def sort_by_title(self, bib_database, descending=False):
+        if descending:
+            bib_database.entries.sort(key= lambda x : x["title"], reverse=True)
+            return bib_database
+        bib_database.entries.sort(key = lambda x : x["title"])
+        return bib_database
+    
+    def filter_by_title(self, bib_database, title : str):
+        bib_result_database = BibDatabase()
+        bib_result_database.entries = [n for n in bib_database.entries if ["title"].lower() == title.lower()]
+        return bib_result_database 
+
+    def sort_by_publisher(self, bib_database, descending=False):
+        if descending:
+            bib_database.entries.sort(key= lambda x : x["publiser"], reverse=True)
+            return bib_database
+        bib_database.entries.sort(key = lambda x : x["publisher"])
+        return bib_database
+    
+    def filter_by_publisher(self, bib_database, publisher : str):
+        bib_result_database = BibDatabase()
+        bib_result_database.entries = [n for n in bib_database.entries if ["publisher"].lower() == publisher.lower()]
+        return bib_result_database 
+
+    def sort_by_entrytype(self, bib_database, descending=False):
+        if descending:
+            bib_database.entries.sort(key= lambda x : x["ENTRYTYPE"], reverse=True)
+            return bib_database
+        bib_database.entries.sort(key = lambda x : x["ENTRYTYPE"])
+        return bib_database
+    
+    def filter_by_entrytype(self, bib_database, entrytype : str):
+        bib_result_database = BibDatabase()
+        bib_result_database.entries = [n for n in bib_database.entries if ["ENTRYTYPE"].lower() == entrytype.lower()]
+        return bib_result_database 
