b405c5f567447a875cc4cd4623413c3542c35456
Teemu Tiljander <teemuti@gmail.com>
2022-12-13 08:34:44 +0200

    Refactored filtering and added tests

--- src/services/filter_service.py
+++ src/services/filter_service.py
@@ -2,39 +2,6 @@ from bibtexparser.bibdatabase import BibDatabase
 class FilterService():
     def __init__(self) -> None:
         pass
-
-    def filter_by_year(self, bib_database, year : int):
-        bib_result_database = BibDatabase()
-        bib_result_database.entries = [n for n in bib_database.entries if int(n["year"]) == year]
-        return bib_result_database
-
-
-    def filter_by_author(self, bib_database, author : str):
-        bib_result_database = BibDatabase()
-        bib_result_database.entries = [n for n in bib_database.entries
-        if n["author"].lower() == author.lower()]
-        return bib_result_database
-
-
-    def filter_by_title(self, bib_database, title : str):
-        bib_result_database = BibDatabase()
-        bib_result_database.entries = [n for n in bib_database.entries
-        if n["title"].lower() == title.lower()]
-        return bib_result_database
-
-
-    def filter_by_publisher(self, bib_database, publisher : str):
-        bib_result_database = BibDatabase()
-        bib_result_database.entries = [n for n in bib_database.entries
-        if n["publisher"].lower() == publisher.lower()]
-        return bib_result_database
-
-
-    def filter_by_entrytype(self, bib_database, entrytype : str):
-        bib_result_database = BibDatabase()
-        bib_result_database.entries = [n for n in bib_database.entries
-        if n["ENTRYTYPE"].lower() == entrytype.lower()]
-        return bib_result_database
  
     def sort_by(self, bib_database, sort_keyword, descending=False):
         if descending:
@@ -42,3 +9,9 @@ class FilterService():
             return bib_database
         bib_database.entries.sort(key = lambda x : x[sort_keyword])
         return bib_database
+
+    def filter_by(self, bib_database, filter_keyword : str, match_case):
+        bib_result_database = BibDatabase()
+        bib_result_database.entries = [n for n in bib_database.entries
+        if filter_keyword in n and n[filter_keyword] == match_case]
+        return bib_result_database
